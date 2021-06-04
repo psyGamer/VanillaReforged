fbd3ffa6707f8a45c2973311dadc75b3248c3452
psyGamer <56753271+psyGamer@users.noreply.github.com>
2021-06-04 10:39:33 +0200

    Added debug blocks

--- src/main/java/dev/psygamer/templetribes/setup/BlockRegistry.java
+++ src/main/java/dev/psygamer/templetribes/setup/BlockRegistry.java
@@ -1,19 +1,49 @@
 package dev.psygamer.templetribes.setup;
 
 import dev.psygamer.templetribes.TempleTribes;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
-public class BlockRegistry {
+import java.util.function.Supplier;
 
+public class BlockRegistry {
+	
 	public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, TempleTribes.MODID);
 	
-//	public static final RegistryObject<Block>
+	public static final RegistryObject<Block> SILVER_ORE = BlockRegistry.register("silver_ore", () ->
+			new Block(AbstractBlock.Properties.create(Material.ROCK)
+					.hardnessAndResistance(3, 10)
+					.sound(SoundType.STONE)
+			));
+	public static final RegistryObject<Block> SILVER_BLOCK = BlockRegistry.register("silver_block", () ->
+			new Block(AbstractBlock.Properties.create(Material.IRON)
+					.hardnessAndResistance(3, 10)
+					.sound(SoundType.METAL)
+			));
+	
 	
 	public static void register() {
-		BLOCKS.register( FMLJavaModLoadingContext.get().getModEventBus());
+		BlockRegistry.BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
+	}
+	
+	private static <T extends Block> RegistryObject<T> registerNoItem(final String name, final Supplier<T> supplier) {
+		return BlockRegistry.BLOCKS.register(name, supplier);
+	}
+	
+	private static <T extends Block> RegistryObject<T> register(final String name, final Supplier<T> supplier) {
+		final RegistryObject<T> block = BlockRegistry.registerNoItem(name, supplier);
+		
+		ItemRegistry.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)));
+		
+		return block;
 	}
 }
--- src/main/java/dev/psygamer/templetribes/setup/ItemRegistry.java
+++ src/main/java/dev/psygamer/templetribes/setup/ItemRegistry.java
@@ -2,15 +2,21 @@ package dev.psygamer.templetribes.setup;
 
 import dev.psygamer.templetribes.TempleTribes;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class ItemRegistry {
-
+	
 	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TempleTribes.MODID);
 	
+	public static final RegistryObject<Item> SILVER_INGOT = ItemRegistry.ITEMS.register("silver_ingot", () -> new Item(new Item.Properties()
+			.group(ItemGroup.MATERIALS)
+	));
+	
 	public static void register() {
-		ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+		ItemRegistry.ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
 	}
 }
